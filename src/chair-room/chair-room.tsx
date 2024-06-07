4d859c6210c8c086134e492a7a2370b0f557716d
0Armaan025 <armaan33000@gmail.com>
2024-06-07 05:46:26 +0530

    feat added footer and stuff

--- src/chair-room/chair-room.tsx
+++ src/chair-room/chair-room.tsx
@@ -8,16 +8,17 @@ const ChairRoomPage: React.FC = () => {
     <div className="chair-room-page">
       <Navbar />
       <div className="main-content">
-        <div className="left-div">
-          <h2>Delegates</h2>
-          <div className="delegates-grid">
+        <div className="left-div rounded-r-md">
+          <h2 className="text-2xl mb-4" style={{ fontFamily: "Poppins" }}>
+            Delegates
+          </h2>
+          <div className="delegates-grid grid-cols-3">
             {Array.from({ length: 20 }, (_, index) => (
               <div key={index} className="delegate">
                 <img
                   src="https://th.bing.com/th?id=OSK.mmcolgx31UinKFJtOKw7hPi2vIQy4EV4nNOsk2Hv9lIJYyv4&w=130&h=100&c=8&o=6&dpr=1.2&pid=SANGAM"
                   alt={`Delegate ${index + 1}`}
                 />
-                <p>Delegate {index + 1}</p>
               </div>
             ))}
           </div>
